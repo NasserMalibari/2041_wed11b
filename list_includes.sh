67e655783db523991cd23e0901162f9c9eed69f3
Nasser Malibari <nassermalibari1@gmail.com>
2024-02-28 14:18:45 +1100

    fixed q4,q8

--- list_includes.sh
+++ list_includes.sh
@@ -3,8 +3,9 @@
 for file in *.c # for file in a.c b.c wc.c uniq.c
 do 
     echo "$file includes:"
-    grep -E "^\s*#include" $file |
-    sed .... |
-    tr .... |
-    
+    grep -E '^#include' "$file" | # grab all #include lines
+    sed -E 's/^#include\s*[<"]/\t/' | # remove the #include and first (" | <) and replace with tab
+    sed -E 's/[>"].*//' | # remove the last (> | ")
+    # tr .... |
+
 done
--- seq.sh
+++ seq.sh
@@ -1,42 +1,66 @@
 #!/bin/sh
 
-
-# if num args = 1
-
-# check first argument is a number
-
-
-
 if [ "$#" -eq 1 ]
 then
     start=1
     increment=1
     end="$1"
-fi
 
-if [ "$#" -eq 2 ] 
+elif [ "$#" -eq 2 ] 
 then
     start="$1"
     increment=1
     end="$2"
+elif [ "$#" -eq 3 ] 
+then
+    start="$1"
+    increment="$2"
+    end="$3"
 fi
 
 
 if [ "$end" -eq "$end"  ] 2> /dev/null
 then
-    echo "all good"
+    :
 else 
     echo "not a number"
     exit 1
 fi
-# if num args = 2
 
+if [ "$increment" -eq "$increment"  ] 2> /dev/null
+then
+    :
+else 
+    echo "not a number"
+    exit 1
+fi
 
+if [ "$start" -eq "$start"  ] 2> /dev/null
+then
+    :
+else 
+    echo "not a number"
+    exit 1
+fi
 
+if [ "$end" -eq "$end"  ] 2> /dev/null
+then
+    :
+else 
+    echo "not a number"
+    exit 1
+fi
+
+if [ "$increment" -eq "$increment"  ] 2> /dev/null
+then
+    :
+else 
+    echo "not a number"
+    exit 1
+fi
 
 
 current=$start
-
 
 # while test $current -le $end
 while [ $current -le $end ]
